56fec711fe2a7c76840ee8aa03719443fb83c404
Max <mchen210@uottawa.ca>
2020-01-22 16:39:51 -0500

    implemented setting page.

--- CertTestApp/app/src/main/java/com/example/certificatetestapp/MainActivity.java
+++ CertTestApp/app/src/main/java/com/example/certificatetestapp/MainActivity.java
@@ -2,17 +2,30 @@ package com.example.certificatetestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getActionBar().hide();
         setContentView(R.layout.activity_main);
+        //Welcome Page action
         Button start=(Button)findViewById(R.id.button);
         Button setting=(Button)findViewById(R.id.button2);
+        final EditText playerName = (EditText)findViewById(R.id.enterName);
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
