108db2548243b6ccd15f87d52679a174ae2c18ba
Max <mchen210@uottawa.ca>
2020-01-22 11:57:08 -0500

    primitive version of Welcome page.

--- CertTestApp/app/src/main/java/com/example/certificatetestapp/MainActivity.java
+++ CertTestApp/app/src/main/java/com/example/certificatetestapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.certificatetestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getActionBar().hide();
         setContentView(R.layout.activity_main);
+        Button start=(Button)findViewById(R.id.button);
+        Button setting=(Button)findViewById(R.id.button2);
     }
 }
