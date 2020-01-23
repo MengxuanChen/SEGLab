61840d8471526fa95bfd9840c5943dbf983d300a
pfarah65 <pfara039@uottawa.ca>
2020-01-23 16:44:27 -0500

    Created SharedData class and added it to settings for updating the data.

--- CertTestApp/app/src/main/java/com/example/certificatetestapp/SettingActivity.java
+++ CertTestApp/app/src/main/java/com/example/certificatetestapp/SettingActivity.java
@@ -3,12 +3,12 @@ package com.example.certificatetestapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.certificatetestapp.SharedData;
 public class SettingActivity extends AppCompatActivity {
     private int num = 10;
     private int grade = 60;
@@ -16,10 +16,18 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        final SharedData sharedData = SharedData.getInstance();
+
+        Log.d("Share",String.valueOf(sharedData.getaPssingGrade()));
+        Log.d("Share",String.valueOf(sharedData.getNumberOfQuestions()));
+
         Button apply= findViewById(R.id.button3);
         Button back= findViewById(R.id.button4);
         final EditText numQuestion = findViewById(R.id.enterNum);
-        final EditText passGrade = findViewById(R.id.enterGrade);
+        final EditText passGrade =  findViewById(R.id.enterGrade);
+
+        numQuestion.setText(String.valueOf(sharedData.getNumberOfQuestions()));
+        passGrade.setText(String.valueOf(sharedData.getaPssingGrade()));
         numQuestion.setFilters(new InputFilter[]{ new InputFilters(5,30)});
         passGrade.setFilters(new InputFilter[]{new InputFilters(0,100)});
 
@@ -34,10 +42,13 @@ public class SettingActivity extends AppCompatActivity {
         apply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num = Integer.parseInt(numQuestion.toString());
-                grade = Integer.parseInt(passGrade.toString());
+                num = Integer.parseInt(((EditText)numQuestion).getText().toString());
+                grade = Integer.parseInt(((EditText)passGrade).getText().toString());
                 Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
+
+                sharedData.setNumberOfQuestions(num);
+                sharedData.setaPssingGrade(grade);
             }
         });
     }
